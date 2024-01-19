ccea705814e3bf2b33e3f126e9eadff7fb813ab1
Chaganti Srujana Gayatri <115392240+5rujana@users.noreply.github.com>
2024-01-19 09:53:02 +0000

    commit

--- basicMaths/primenum.cpp
+++ basicMaths/primenum.cpp
@@ -1,2 +1,36 @@
 #include<iostream>
-#include<bits/cstd++.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc , char * argv[]){
+    if (argc !=2)
+    {
+        cout<< "Usage: "<< argv[0] << " <n> "<<endl;
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    int count=0;
+    
+    for (int i = 1; i*i <= n ; i++)
+    {
+        if(n%i==0){
+            count++;
+            if (n/i !=i)
+            {
+                count++;
+            }
+            
+        }
+    }
+
+    if (count==2)
+    {
+        cout<< "Prime number" << endl;
+    }else{
+        cout<< "Not Prime number" << endl;
+    }
+    
+    
+}
